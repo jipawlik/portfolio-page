2602698d0bb71c7a9aedb6fda56b4a6673778b76
jipawlik <ji-pawlik@tlen.pl>
2021-08-05 11:29:09 +0200

    burger menu for testing

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,17 @@
 import { Link } from "react-router-dom";
+import { Fade as Hamburger } from "hamburger-react";
+import { useState } from "react";
+import { disableBodyScroll, enableBodyScroll } from "body-scroll-lock";
 
 const Nav = () => {
+  const [isOpen, setOpen] = useState(false);
+  const hideMenu = () => {
+    if (window.innerWidth < 1024) {
+      setOpen(!isOpen);
+    }
+  };
+  isOpen ? disableBodyScroll(document) : enableBodyScroll(document);
+
   return (
     <nav className="navigation">
       <div className="logo">
@@ -9,18 +20,32 @@ const Nav = () => {
           jp.
         </Link>
       </div>
-
-      <div className="nav-items">
-        <Link to={"/"} className="nav-item">
-          Home
-        </Link>
-        <Link to={"/projects"} className="nav-item">
-          Projects
-        </Link>
-        <Link to={"/contact"} className="nav-item">
-          Contact
-        </Link>
+      <div className="nav-burger">
+        <Hamburger
+          size={40}
+          color="#141414"
+          label="Show menu"
+          toggled={isOpen}
+          toggle={setOpen}
+        />
       </div>
+      <ul className={isOpen ? "burger-menu" : "nav-items"}>
+        <li className="nav-item">
+          <Link to={"/"} className="nav-link" onClick={hideMenu}>
+            Home
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to={"/projects"} className="nav-link" onClick={hideMenu}>
+            Projects
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to={"/contact"} className="nav-link" onClick={hideMenu}>
+            Contact
+          </Link>
+        </li>
+      </ul>
     </nav>
   );
 };
